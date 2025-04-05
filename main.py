b28794a6baece878c03fe510070a55b54cea50ce
BinC0de <bwcoding0@gmail.com>
2025-04-05 15:44:19 +0200

    Refactor: get words func is now in imported via seperate file

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+from stats import get_words_in_text
+
 def get_book_text(filepath):
     with open(filepath) as book:
         return book.read()  
-
-def get_words_in_text(text):
-    num_words = len(text.split())
-    return f"{num_words} words found in the document"
 
 def main():
     book_path = "books/frankenstein.txt"
