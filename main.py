07315d29bc5f9819becd13288754f8573422b1f9
BinC0de <bwcoding0@gmail.com>
2025-04-05 15:40:14 +0200

    Feat: get amount of words in a text

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 def get_book_text(filepath):
     with open(filepath) as book:
-        return book.read()      
+        return book.read()  
+
+def get_words_in_text(text):
+    num_words = len(text.split())
+    return f"{num_words} words found in the document"
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    total_words = get_words_in_text(text)
+    print(total_words)
 
 main()
