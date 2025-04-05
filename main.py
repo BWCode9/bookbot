18e8d2d808faac542b3ffab97c0c1173abbe3109
BinC0de <bwcoding0@gmail.com>
2025-04-05 15:21:15 +0200

    Feat: get book text

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as book:
+        return book.read()      
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+main()
