ab065b6e5ce7358cd9e3f0331175953404d70921
BinC0de <bwcoding0@gmail.com>
2025-04-07 17:20:52 +0200

    Feat: Function that returns a dictionary of characters

--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def get_words_in_text(text):
     num_words = len(text.split())
     return f"{num_words} words found in the document"
+
+def get_character(words):
+    characters = {}
+    for character in words:
+        lowercase_letter = character.lower()
+        if lowercase_letter in characters:
+            characters[lowercase_letter] +=1
+        else:
+            characters[lowercase_letter] = 1
+    return characters
